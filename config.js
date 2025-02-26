ced8dd4e29c78e0da25219b0c1c7a54183a3761a
HRBsupport <wakeoneteam@gmail.com>
2025-02-26 23:53:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 require('dotenv').config(); 
 
 global.owner = process.env.OWNER_NUMBER.split(','); 
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
-global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
-global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.anitav4 = process.env.BOT_NAME || "HONORSBOT-𝐕𝟒";
+global.ownername = process.env.OWNER_NAME || "ZERO OF HONORS";
+global.packname = process.env.PACK_NAME || "HONORSBOT-𝐕𝟒";
+global.author = process.env.AUTHOR || "HONORSBOT";
+global.channelname = process.env.CHANNEL_NAME || "HONORSBOT";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
 global.autoTyping = process.env.AUTO_TYPING === 'true'; 
 global.autoRecord = process.env.AUTO_RECORD === 'true';
